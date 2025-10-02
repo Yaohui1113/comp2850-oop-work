b2398a397a52ad91eba64938ef2eb9c4b73f933d
Yaohui1113 <3376346848@qq.com>
2025-10-02 15:16:42 +0000

    1

--- tasks/task3_1/Args.kt
+++ tasks/task3_1/Args.kt
@@ -1,1 +1,18 @@
 // Task 3.1: command line arguments
+import kotlin.math.PI
+import kotlin.system.exitProcess
+
+fun main(args: Array<String>) {
+    if (args.size != 1) {
+        println("Error: filename required on command line!")
+        exitProcess(1)
+    }
+
+    val r = args[0].toDouble()
+    when{
+        r > 0.0 -> println("Area = ${"%.3f".format(PI * r*r)}")
+        else    ->println("Invalid radius!")
+    }
+
+    // required argument available as args[0]
+}
